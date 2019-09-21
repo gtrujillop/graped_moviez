e8c29d8e082f002296e3ad66fae404dc7fe165f4
Gaston Trujillo <gaston.trujillo.java@gmail.com>
2019-09-21 12:56:00 -0500

    Initial commit with all gem folders and files

---
+++ lib/graped_moviez.rb
@@ -0,0 +1,6 @@
+require "graped_moviez/version"
+
+module GrapedMoviez
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/graped_moviez/version.rb
@@ -0,0 +1,3 @@
+module GrapedMoviez
+  VERSION = "0.1.0"
+end
